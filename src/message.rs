881ac9dd50c298814bb34da976b7195b7428e74e
Nayyara Airlangga <nayyara.airlangga@gmail.com>
2022-11-19 15:46:12 +0700

    Remove invalid variant from http method enum

--- src/message.rs
+++ src/message.rs
@@ -1,24 +1,14 @@
-use std::{error::Error, fmt::Display, str::FromStr};
+use std::str::FromStr;
 
 #[derive(Debug)]
 pub enum HttpMethod {
     Get,
-    Invalid,
 }
-
-impl Display for HttpMethod {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Invalid HTTP Method")
-    }
-}
-
-impl Error for HttpMethod {}
 
 impl AsRef<str> for HttpMethod {
     fn as_ref(&self) -> &str {
         match *self {
             Self::Get => "GET",
-            Self::Invalid => "INVALID",
         }
     }
 }
