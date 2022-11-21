b37dd3e89c9aaeb3340039a009fab3182e8ee1c3
Nayyara Airlangga <nayyara.airlangga@gmail.com>
2022-11-22 00:54:53 +0700

    Add routes for showcase in main

--- src/main.rs
+++ src/main.rs
@@ -18,9 +18,18 @@ async fn post_handler(req: HttpRequest) -> HttpResponse {
     res
 }
 
+async fn index(_req: HttpRequest) -> HttpResponse {
+    let mut res = HttpResponse::new(HttpStatus::OK);
+    res.set_header("Content-Type", "text/plain");
+    res.set_body("OK!");
+    res
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let router = Router::new().route("/mabar", get(get_handler).post(post_handler));
+    let router = Router::new()
+        .route("/", get(index))
+        .route("/mabar", get(get_handler).post(post_handler));
 
     let server = HttpServer::new()
         .serve(router)
